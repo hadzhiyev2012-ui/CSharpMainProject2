158296eaed54db2866001a278b9e954e76253163
Ансар <khadzhiyev2012@inbox.ru>
2026-09-19 21:15:56 +0300

    Дз 1 модуль

--- Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
+++ Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEditor.Graphs;
+using UnityEngine;
 
 namespace Model.Runtime.Projectiles
 {
@@ -29,12 +30,12 @@ namespace Model.Runtime.Projectiles
             ///////////////////////////////////////
             // Insert you code here
             ///////////////////////////////////////
-
-
+            float maxHeight = totalDistance * 0.6f;
+            localHeight = maxHeight * (-(t - 1) * (t * 2 - 1) + 1);
             ///////////////////////////////////////
             // End of the code to insert
             ///////////////////////////////////////
-            
+
             Height = localHeight;
             if (time > StartTime + _timeToTarget)
                 Hit(_target);
